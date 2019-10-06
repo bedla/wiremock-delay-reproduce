87654c5bbb7f7ea3bf0a6bcacdf50ee96167a99a
Ivo Smid <ivo.smid@gmail.com>
2019-10-06 15:54:03 +0200

    do better logging (and try to reset stubs each run)

--- src/test/java/cz/bedla/wiremock/DelayReproduceTest.java
+++ src/test/java/cz/bedla/wiremock/DelayReproduceTest.java
@@ -4,6 +4,8 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.google.common.base.Charsets;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,6 +22,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DelayReproduceTest {
+    private static final Logger log = LoggerFactory.getLogger(DelayReproduceTest.class);
+
     private WireMockServer wireMockServer;
 
     @BeforeAll
@@ -36,33 +40,51 @@ class DelayReproduceTest {
         wireMockServer.stop();
     }
 
+    @BeforeEach
+    void reset() {
+        log.info("reset");
+        wireMockServer.resetAll();
+    }
+
     @Test
     @Order(1)
     void timeout() throws IOException {
+        log.info("test timeout start");
+
         wireMockServer.stubFor(get(urlEqualTo("/timeout"))
                 .willReturn(aResponse()
                         .withFixedDelay(10 * 1000)
                         .withBody("body1")));
 
         downloadContentAndMeasure("/timeout", null);
+
+        log.info("test timeout end");
     }
 
     @Test
     @Order(2)
     void content() throws IOException {
+        log.info("test content start");
+
         wireMockServer.stubFor(get(urlEqualTo("/content"))
                 .willReturn(aResponse()
                         .withBody("body2")));
+        log.info("test content stub");
 
         downloadContentAndMeasure("/content", "body2");
+
+        log.info("test content end");
     }
 
     // HTTPS tests here
 
     private void downloadContentAndMeasure(String urlDir, String expectedBody) throws IOException {
+        log.info("downloadContentAndMeasure urlDir={}", urlDir);
+
         final long start = System.currentTimeMillis();
 
-        System.out.println(wireMockServer.baseUrl());
+
+        boolean exceptionOccurred = false;
         try {
             final String url = "http://localhost:" + wireMockServer.port() + urlDir;
             final HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
@@ -75,8 +97,14 @@ class DelayReproduceTest {
                 final String body = httpGetContent(connection);
                 assertEquals(expectedBody, body);
             }
+        } catch (Exception e) {
+            exceptionOccurred = true;
+            log.info("exception '{}' after ms {}", e.getMessage(), TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - start));
+            throw e;
         } finally {
-            System.out.println(TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - start));
+            if (!exceptionOccurred) {
+                log.info("downloaded at ms {}", TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - start));
+            }
         }
     }
 
